1b172e70fdc3ab5bd2951fea508eda0d8f98cf68
bassijtsma <btsijtsma@gmail.com>
2017-01-23 23:28:02 +0100

    experiment with synchronous request

--- src/async/Async.java
+++ src/async/Async.java
@@ -24,16 +24,23 @@ public class Async {
         HttpGet request = new HttpGet(url);
 
         request.addHeader("User-Agent", USER_AGENT);
+        System.out.println("pre execute");
         HttpResponse response = client.execute(request);
+        System.out.println("post execute");
 
         System.out.println("Response code" + response.getStatusLine().getStatusCode());
+        System.out.println("post getting reponse code");
+
         BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        System.out.println("created buffered reader");
+
         StringBuffer result = new StringBuffer();
         String line = "";
         while ((line = rd.readLine()) != null) {
             result.append(line);
 //            System.out.println(line);
         }
+        System.out.println("done");
 
     }
 }
