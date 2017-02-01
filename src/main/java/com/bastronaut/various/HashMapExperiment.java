af079fb30d110bad9baa1ae3ab8e47b2c1b7aeb9
bassijtsma <btsijtsma@gmail.com>
2017-02-01 22:26:13 +0100

    fail getting google caliper to work

--- src/main/java/com/bastronaut/various/HashMapExperiment.java
+++ src/main/java/com/bastronaut/various/HashMapExperiment.java
@@ -1,11 +1,19 @@
 package com.bastronaut.various;
 
+import com.google.caliper.Param;
+import com.google.caliper.SimpleBenchmark;
 import java.util.HashMap;
 
 /**
  * Created by BSijtsma on 01-02-2017.
+ *
+ * Experiment with hash collision when placing hashcodes in bins and its effect on performance
+ * in a hashmap
+ *
+ * example from: http://www.nurkiewicz.com/2014/04/hashmap-performance-improvements-in.html
+ * benchmark using google caliper
  */
-public class HashMapExperiment {
+public class HashMapExperiment extends SimpleBenchmark {
 
     public static void hashMapToString() {
         HashMap<String, String> testMap = new HashMap<>();
@@ -13,6 +21,10 @@ public class HashMapExperiment {
         testMap.put("surname", "henk");
         testMap.put("color", "Blue");
         System.out.println(testMap.toString());
+    }
+
+    @Benchmark
+    public static void testHashMapGet() {
 
     }
 }
