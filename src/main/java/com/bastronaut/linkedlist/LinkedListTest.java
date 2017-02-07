6066673ca87a0ee5a0598df2a2efc718bb5a79c2
bassijtsma <btsijtsma@gmail.com>
2017-02-07 08:53:52 +0100

    Adds quick testcase for LL

--- src/main/java/com/bastronaut/linkedlist/LinkedListTest.java
+++ src/main/java/com/bastronaut/linkedlist/LinkedListTest.java
@@ -8,19 +8,22 @@ import static org.junit.Assert.*;
  * Created by BSijtsma on 07-02-2017.
  */
 public class LinkedListTest {
-    @Test
-    public void push() throws Exception {
-
-    }
 
     @Test
     public void pop() throws Exception {
-
-    }
-
-    @Test
-    public void getSize() throws Exception {
-
+        LinkedList<String> ll = new LinkedList<>();
+        ll.push("Hi");
+        assertTrue(ll.pop().equals("Hi"));
+        ll.push("1");
+        ll.push("2");
+        ll.push("3");
+        assertTrue(ll.pop().equals("3"));
+        assertTrue(ll.getSize() == (2));
+        assertTrue(ll.pop().equals("2"));
+        assertFalse(ll.isEmpty());
+        assertTrue(ll.pop().equals("1"));
+        assertTrue(ll.getSize() == (0));
+        assertTrue(ll.isEmpty());
     }
 
 }
